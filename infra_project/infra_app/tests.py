eae72c6228576f7d1a64628d1c99a0204d1b1c56
Максим Бастрыкин <maksimbastrikin@bk.ru>
2021-11-06 18:49:40 +0300

    Add: django tests in workflow

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -10,15 +10,15 @@ class StaticPagesURLTests(TestCase):
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
         response = self.guest_client.get('/')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.status_code, 200)
 
-        response = self.guest_client.get('/second_page/')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+        response = self.guest_client.get('/second/')
+        self.assertEqual(response.status_code, HTTPStatus.OK.value)
 
     def test_page_shows_correct_context(self):
         """Проверка контекста страниц."""
         response = self.guest_client.get('/')
         self.assertContains(response, 'У меня получилось!')
 
-        response = self.guest_client.get('/second_page/')
-        self.assertContains(response, 'А это вторая страница!')
+        response = self.guest_client.get('/second/')
+        self.assertContains(response, 'А это вторая страница')
